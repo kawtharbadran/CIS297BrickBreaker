870edafbfe388a80df0d57690b93f6b96699420d
Kawthar Badran <37708354+kawtharbadran@users.noreply.github.com>
2019-04-20 16:54:22 -0400

    added spriteSheet

---
+++ BrickBreaker/BrickBreaker/SpriteSheet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace BrickBreaker
+{
+    public static class SpriteSheet
+    {
+        public static Size iconSize => new Size(80, 79);
+
+        public static Point shorterPaddleLocation => new Point(0, 0);
+        public static Point slowerBallLocation => new Point(0, 80);
+        public static Point slowerPaddleLocation => new Point(0, 160);
+        public static Point longerPaddleLocation => new Point(0, 79);
+        public static Point fasterBallLocation => new Point(80, 79);
+        public static Point fasterPaddleLocaiton => new Point(160, 79);
+    }
+}
